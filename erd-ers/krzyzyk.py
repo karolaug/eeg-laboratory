dfc3aef0448adbfdfc885f4c4c1d9e422f6d09af
Karol Augustin <karol@augustin.pl>
2012-04-23 11:30:56 +0200

    Added license information, minor improvment

--- erd-ers/krzyzyk.py
+++ erd-ers/krzyzyk.py
@@ -1,12 +1,32 @@
+#!/usr/bin/python -u
 
-#Fire with -u switch,
-#python -u filename.py
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# Author:
+#     Karol Augustin <karol@augustin.pl>
+# git repository: http://git.nimitz.pl
+
+'''
+Fire with -u switch,
+python -u filename.py
+'''
 
 import time
 import sys
 import numpy as np
 
-ile = 10
+ile = int(sys.argv[1])
 sys.stdout.write("\n\n\n\n")
 sys.stdout.write(" ")
 for i in range(ile):
@@ -15,5 +35,7 @@ for i in range(ile):
     time.sleep(2)
     sys.stdout.write("\r ")
 
-
+time.sleep(5)
 sys.stdout.write("\n")
+
+
