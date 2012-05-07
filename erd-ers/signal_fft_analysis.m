c4ddcd4740dc882ae3d5c8789c2d637594113d15
Karol Augustin <karol@augustin.pl>
2012-05-07 23:54:25 +0200

    Add plots to fft and periodogram analysis

--- erd-ers/signal_fft_analysis.m
+++ erd-ers/signal_fft_analysis.m
@@ -9,5 +9,20 @@ fft_exp = real(squeeze(mean(data_exp_fft,1)));
 fft_ref = real(squeeze(mean(data_ref_fft,1)));
 
 
-%plot(t, fft_exp(9,:));
-plot(t, fft_ref(9,:));
+subplot(2,2,1)
+plot(t,fft_exp(9,:),'r',t,fft_ref(9,:),'b')
+axis([5,15,0,1500])
+title('elektroda C3');
+subplot(2,2,2)
+plot(t,fft_exp(11,:),'r',t,fft_ref(11,:),'b')
+axis([5,15,0,1500])
+title('elektroda C4');
+
+subplot(2,2,3)
+plot(t,fft_exp(9,:) - fft_ref(9,:),'b')
+axis([5,15,0,200])
+title('roznica C3');
+subplot(2,2,4)
+plot(t,fft_exp(11,:) - fft_ref(11,:),'b')
+axis([5,15,0,200])
+title('roznica C4');
--- erd-ers/signal_periodogram_analysis.m
+++ erd-ers/signal_periodogram_analysis.m
@@ -12,4 +12,21 @@ end
 per_exp = real(squeeze(mean(data_exp_per,1)));
 per_ref = real(squeeze(mean(data_ref_per,1)));
 
-plot(t,per_exp(9,:))
+
+subplot(2,2,1)
+plot(t,per_exp(9,:),'r',t,per_ref(9,:),'b')
+axis([5,15,0,1500])
+title('elektroda C3');
+subplot(2,2,2)
+plot(t,per_exp(11,:),'r',t,per_ref(11,:),'b')
+axis([5,15,0,1500])
+title('elektroda C4');
+
+subplot(2,2,3)
+plot(t,per_exp(9,:) - per_ref(9,:),'b')
+axis([5,15,0,150])
+title('roznica C3');
+subplot(2,2,4)
+plot(t,per_exp(11,:) - per_ref(11,:),'b')
+axis([5,15,0,150])
+title('roznica C4');
