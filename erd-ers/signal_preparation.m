abb40d60359fa4c4c9bcdff6cec3c2c93e3b6dd0
Karol Augustin <karol@augustin.pl>
2012-05-08 14:44:12 +0200

    Fix frequency scale

--- erd-ers/signal_preparation.m
+++ erd-ers/signal_preparation.m
@@ -6,7 +6,7 @@ data = sv_sig2trigg_bool(data, 22);
 %plot(data(22,:));
 q = find(data(22,:) == 1);
 data = detrend(data);
-t = linspace(1,256, 513);
+t = linspace(0,256, 513);
 data = bsxfun(@minus,data,(data(20,:)+data(21,:)/2));
 [data_ref, data_exp] = prepare_data(data, q, info);
 save([h 'eeg-laboratory/erd-ers/data_non_filter.mat'],'data_ref', 'data_exp');
